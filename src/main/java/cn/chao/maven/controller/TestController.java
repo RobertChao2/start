4140d4ecf7466b038a4c359ecf0d02aee4324d92
RobertChao <3102913461@qq.com>
2019-07-10 20:27:39 +0800

    Spring Boot 新建完成后台的第一次提交，目前登录后可以访问 /hello，后期增加更多内容。

---
+++ src/main/java/cn/chao/maven/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.chao.maven.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("hello")
+    public String getHello(){
+        return "hello , Spring Boot !!! I think you are a good start .";
+    }
+}
